f34148690b75f95320ee3f08a6b6374d358df4ca
rafapm1999 <rportillom99.code@gmail.com>
2023-04-24 21:17:54 +0200

    video 50

--- expenses/src/components/items/ItemCost.js
+++ expenses/src/components/items/ItemCost.js
@@ -1,6 +1,22 @@
 import './ItemCost.css';
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {
+  faArrowDownLong, 
+  faArrowUpLong, 
+  } from "@fortawesome/free-solid-svg-icons"
 
-function ItemCost() {
+function ItemCost(props) {
+
+  const sign = {
+    plus:{
+      entity: '&#43',
+      unicode: '\u002b'
+    },
+    minus:{
+      entity: '&#43',
+      unicode: '\u002b'
+    }
+  }
   return (
     <div className="item-cost flex-20 expense fa-3x flex-center">
       <span>&#8722; $20</span>
--- expenses/src/components/items/ItemIcon.js
+++ expenses/src/components/items/ItemIcon.js
@@ -6,7 +6,12 @@ function ItemIcon(props) {
     <div className="item-icon fa-4x flex-20 flex-center">
       <span className="fa-layers fa-fw fa-xl">
         <FontAwesomeIcon icon={props.icons[0]} className='light-bill'/>
-        <FontAwesomeIcon icon={props.icons[1]} inverse  transform="shrink-8" className='bill'/>
+        <FontAwesomeIcon 
+          icon={props.icons[1]} 
+          inverse  
+          transform="shrink-8" 
+          className='bill'
+        />
       </span>
     </div>
   );
