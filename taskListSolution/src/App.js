9d8983776155522799128e70276ae23d6d1ded1a
rafapm1999 <rportillom99.code@gmail.com>
2023-05-03 19:25:12 +0200

    Starting TaskListSolution

---
+++ taskListSolution/src/App.js
@@ -0,0 +1,13 @@
+//import './App.css';
+import { data } from "./mockedDB/tasks.json"
+
+function App() {
+  return (
+    <main>
+      <section></section>
+      <section></section>
+    </main>
+  );
+}
+
+export default App;
