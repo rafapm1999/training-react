74cdaf06234cd1caf2e7244b40eb01eec52a91d6
rafapm1999 <rportillom99.code@gmail.com>
2023-05-08 16:35:17 +0200

    preparing and resolving new branch problems

--- clean-template/src/App.js
+++ clean-template/src/App.js
@@ -1,11 +1,12 @@
 //import './App.css';
 
 function App() {
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>Lorem Ipsum dolor si tamet consectetur adipisicing elit.</p>
-    </div>
+  <div> 
+     <h1>Hello World!</h1>
+     <p>Lorem Ipsum dolor si tamet consectetur adipisicing elit.</p>
+  </div>
   );
 }
 
